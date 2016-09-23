6dbdb7c8fc5137d0c660ad0775706c70684b2121
Zeqing Zhang <zzq889@gmail.com>
2016-09-24 01:00:58 +0800

    /upload api

--- src/index.js
+++ src/index.js
@@ -1,20 +1,60 @@
+import express from 'express';
+import morgan from 'morgan';
 import path from 'path';
-import { openImage } from './utils';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { processImage, downloadImage } from './utils';
 
 
-const imagePath = path.join(__dirname, '../upload', 'image.jpg');
-const logoPath = path.join(__dirname, 'logo.png');
+/**
+ * init app
+ */
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('combined'));
 
-Promise.all([openImage(imagePath), openImage(logoPath)])
-.then(([image, logo]) => {
-  image.batch()
-  .rotate(90, 'white')
-  .crop(200, 200)
-  .paste(0, 0, logo)
-  .writeFile(path.join(__dirname, '../output', 'image.jpg'), (e) => {
-    if (e) {
-      console.log(e);
-    }
+/**
+ * provide root route
+ */
+app.get('/', (req, res) => {
+  res.json({
+    message: 'hello',
   });
 });
 
+app.post('/upload', (req, res) => {
+  // mobile, name, serverId
+  const { serverId } = req.body;
+  const accessToken = '';
+  const weixinApi = 'http://file.api.weixin.qq.com/cgi-bin/media/get';
+  const imageUrl = `${weixinApi}?access_token=${accessToken}&media_id=${serverId}`;
+  downloadImage(imageUrl)
+  .then(file => processImage(file))
+  .then(imageName => res.json({
+    image: `/image/${imageName}`,
+  }))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  });
+});
+
+app.get('/image/:uid', (req, res) => {
+  const uid = req.params.uid;
+  res.sendFile(uid, { root: path.join(__dirname, '../output') });
+});
+
+
+/**
+ * start app
+ */
+const server = app.listen(3001, () => {
+  const port = server.address().port;
+  console.log(`app listening on http://localhost:${port}`);
+});
+
+export default server;
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 import lwip from 'lwip';
+import path from 'path';
+import moment from 'moment';
 
-export function openImage(file) {
+function openImage(file) {
   return new Promise((resolve, reject) => {
     lwip.open(file, (err, image) => {
       if (err) {
@@ -11,4 +13,52 @@ export function openImage(file) {
       resolve(image);
     });
   });
+}
+
+export function processImage({ name, filePath }) {
+  const logoPath = path.join(__dirname, 'logo.png');
+  const pad = 40;
+  const spacing = 30;
+
+  return new Promise((resolve, reject) => {
+    Promise.all([openImage(filePath), openImage(logoPath)])
+    .then(([image, logo]) => {
+      const bottomPos = spacing + logo.height();
+      const bottomPad = bottomPos + spacing;
+
+      image.cover(960, 1440, (coverErr, croppedImage) => {
+        croppedImage.writeFile(path.join(__dirname, '../output', name), (e) => {
+          if (e) {
+            reject(e);
+          }
+        });
+
+        croppedImage.extract(pad, pad, 960 - pad, 1440 - bottomPad, (extractErr, img) => {
+          const outputName = name.replace(/\.[0-9a-z]+$/i, '_frame$&');
+          const outputPath = path.join(__dirname, '../output', outputName);
+          img.batch()
+          .crop(960 - pad - pad, 1440 - pad - bottomPad)
+          .pad(pad, pad, pad, bottomPad, 'white')
+          .paste(960 - logo.width() - pad, 1440 - bottomPos, logo)
+          .writeFile(outputPath, (e) => {
+            if (e) {
+              reject(e);
+            }
+            resolve(outputName);
+          });
+        });
+      });
+    })
+    .catch(e => reject(e));
+  });
+}
+
+export function downloadImage(url) {
+  // TODO: download and save image
+  const name = moment().format();
+
+  return {
+    name,
+    filePath,
+  };
 }
